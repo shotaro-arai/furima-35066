eb7d00a6219220e8e0b2a0468e2d79c11d7b67cb
shotaro-arai <syoooth0720@yahoo.co.jp>
2021-03-30 16:09:16 +0900

    バリデーションとアソシエーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,17 @@
 class Item < ApplicationRecord
+  
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :category_id
+    validates :condition_id 
+    validates :delivery_fee_id
+    validates :prefectures_id
+    validates :term_to_send_id 
+    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999} 
+  end
+  validates :user, foreign_key: true
+
+
+  belongs_to :user
 end
