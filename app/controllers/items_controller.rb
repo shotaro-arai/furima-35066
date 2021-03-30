6b866cc4b3a256c4d32ade9874be2196c63a20aa
shotaro-arai <syoooth0720@yahoo.co.jp>
2021-03-30 22:33:09 +0900

    createアクションにデータの保存に成功したか否かの条件分岐を記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,7 +8,12 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    @item.save
+    if @item.save
+      redirect_to root_path
+    else
+      @item = Item.new(item_params)
+      render 'new'
+    end
   end
 
   private
