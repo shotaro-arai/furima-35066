6cac6954aadead3ba1f9c8050eb50658311710fb
shotaro-arai <syoooth0720@yahoo.co.jp>
2021-03-30 17:51:36 +0900

    ActiveHashの導入、記述

---
+++ app/models/term_to_send.rb
@@ -0,0 +1,8 @@
+class Term_to_send < ActiveHash::Base
+  self.date = [
+    {id: 1, name: "---"},
+    {id: 2, name: "１~２日で発送"},
+    {id: 3, name: "２~３日で発送"},
+    {id: 3, name: "４~７日で発送"}
+  ]
+end
