f51541f490b9dc72b72b26723d5cb7e62ba3fc0a
shotaro-arai <syoooth0720@yahoo.co.jp>
2021-03-30 19:10:06 +0900

    ビューにActiveHashを読み込めるように編集。それに伴うミスも修正。

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,16 +3,15 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :description
-    with_options numericality: {other_than: 1}
-    validates :category_id
-    validates :condition_id 
-    validates :delivery_fee_id
-    validates :prefectures_id
-    validates :term_to_send_id 
+    with_options numericality: {other_than: 1} do
+      validates :category_id
+      validates :condition_id 
+      validates :delivery_fee_id
+      validates :prefectures_id
+      validates :term_to_send_id
+    end
     validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999} 
   end
-  validates :user, foreign_key: true
-
 
   belongs_to :user
 
--- app/models/term_to_send.rb
+++ app/models/term_to_send.rb
@@ -1,5 +1,5 @@
 class Term_to_send < ActiveHash::Base
-  self.date = [
+  self.data = [
     {id: 1, name: "---"},
     {id: 2, name: "１~２日で発送"},
     {id: 3, name: "２~３日で発送"},
