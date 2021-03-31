8957a6b0661003ec3d1346c41cd83c36d224d613
shotaro-arai <syoooth0720@yahoo.co.jp>
2021-03-31 14:27:53 +0900

    ActiveHashを用いたカラムに、idが１の場合のテストコードをそれぞれ追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -35,10 +35,22 @@ RSpec.describe Item, type: :model do
       expect(@item.errors.full_messages).to include("Category can't be blank")
     end
 
+    it "category_idが1の場合は商品出品できない" do
+      @item.category_id = '1'
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category must be other than 1")
+    end
+
     it "商品の状態がないと商品出品できない" do
       @item.condition_id = ''
       @item.valid?
       expect(@item.errors.full_messages).to include("Condition can't be blank")
+    end
+
+    it "condition_idが1の場合は商品出品できない" do
+      @item.condition_id = '1'
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Condition must be other than 1")
     end
 
     it "配送料の負担がないと商品出品できない" do
@@ -47,16 +59,34 @@ RSpec.describe Item, type: :model do
       expect(@item.errors.full_messages).to include("Delivery fee can't be blank")
     end
 
+    it "delivery_fee_idが1の場合は商品出品できない" do
+      @item.delivery_fee_id = '1'
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Delivery fee must be other than 1")
+    end
+
     it "発送元の地域がないと商品出品できない" do
       @item.prefectures_id = ''
       @item.valid?
       expect(@item.errors.full_messages).to include("Prefectures can't be blank")
     end
 
+    it "prefectures_idが1の場合は商品出品できない" do
+      @item.prefectures_id = '1'
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Prefectures must be other than 1")
+    end
+
     it "発送までの日数がないと商品出品できない" do
       @item.term_to_send_id = ''
       @item.valid?
       expect(@item.errors.full_messages).to include("Term to send can't be blank")
+    end
+
+    it "term_to_send_idが1の場合は商品出品できない" do
+      @item.term_to_send_id = '1'
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Term to send must be other than 1")
     end
 
     it "販売価格がないと商品出品できない" do
