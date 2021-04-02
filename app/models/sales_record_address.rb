b66bf57ad4a7d9994bdb62c77d2ac133caf49b17
shotaro-arai <syoooth0720@yahoo.co.jp>
2021-04-02 14:45:53 +0900

    formオブジェクトにデータを保存する処理を記述

--- app/models/sales_record_address.rb
+++ app/models/sales_record_address.rb
@@ -1,6 +1,6 @@
 class SalesRecordAddress
   include ActiveModell::Model
-  attr.accessor :post_code, :prefectures_id, :city, :address_number, :building, :phone_number, :sales_record_id, :item_id, :user_id
+  attr.accessor :post_code, :prefectures_id, :city, :address_number, :building, :phone_number, :item_id, :user_id
 
   with_options presence: true do
     validates :post_code, format: {with:/\A[0-9]{3}-[0-9]{4}\z/i}
@@ -8,13 +8,13 @@ class SalesRecordAddress
     validates :city
     validates :address_number
     validates :phone_number, format: {with:/\A[0-9]{9}\z/i}
-    validates :sales_record_id
     validates :item_id
     validates :user_id
   end
 
-
   def save
+    sales_record = SalesRecord.create(item_id: item_id, user_id: user_id)
+    Adress.create(post_code: post_code, prefectures_id: prefectures_id, city: city, address_number: address_number, phone_number: phone_number, sales_record_id: sales_record.id)
   end
 
 end
