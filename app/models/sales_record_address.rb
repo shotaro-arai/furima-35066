7a8e0070211b0f5691ae4f5c1e0ac12c965bde73
shotaro-arai <syoooth0720@yahoo.co.jp>
2021-04-02 14:39:10 +0900

    formオブジェクトにバリデーションの記述

--- app/models/sales_record_address.rb
+++ app/models/sales_record_address.rb
@@ -2,6 +2,18 @@ class SalesRecordAddress
   include ActiveModell::Model
   attr.accessor :post_code, :prefectures_id, :city, :address_number, :building, :phone_number, :sales_record_id, :item_id, :user_id
 
+  with_options presence: true do
+    validates :post_code, format: {with:/\A[0-9]{3}-[0-9]{4}\z/i}
+    validates :prefectures_id, numericaliy: {other_than: 1}
+    validates :city
+    validates :address_number
+    validates :phone_number, format: {with:/\A[0-9]{9}\z/i}
+    validates :sales_record_id
+    validates :item_id
+    validates :user_id
+  end
+
+
   def save
   end
 
