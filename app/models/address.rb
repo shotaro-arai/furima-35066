9146bab0158248465d233e299ab407fc0c142fae
shotaro-arai <syoooth0720@yahoo.co.jp>
2021-04-02 12:47:19 +0900

    購入者情報のアソシエーションの記述

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,3 @@
 class Address < ApplicationRecord
+  belongs_to :sales_record
 end
--- app/models/sales_record.rb
+++ app/models/sales_record.rb
@@ -1,4 +1,5 @@
 class SalesRecord < ApplicationRecord
   belongs_to :user
   belongs_to :item
+  has_one :address
 end
