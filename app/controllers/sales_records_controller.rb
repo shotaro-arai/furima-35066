a51e76333e3f7787c24adb721b5b205148d9c8d4
shotaro-arai <syoooth0720@yahoo.co.jp>
2021-04-05 10:56:18 +0900

    コントローラーにトークンに対するストロングパラメーター、Payjpへのトークン決済情報送信メソッドを記述

--- app/controllers/sales_records_controller.rb
+++ app/controllers/sales_records_controller.rb
@@ -10,6 +10,7 @@ class SalesRecordsController < ApplicationController
   def create
     @sales_record_address = SalesRecordAddress.new(sales_record_address_params)
     if @sales_record_address.valid?
+      pay_item
       @sales_record_address.save
       redirect_to root_path
     else
@@ -21,7 +22,7 @@ class SalesRecordsController < ApplicationController
   private
 
   def sales_record_address_params
-    params.require(:sales_record_address).permit(:post_code, :prefectures_id, :city, :address_number, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:sales_record_address).permit(:post_code, :prefectures_id, :city, :address_number, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
   def move_action
@@ -29,5 +30,14 @@ class SalesRecordsController < ApplicationController
     if current_user.id == @item.user_id || user_signed_in? && @item.sales_record.present?
       redirect_to root_path
     end
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: Item.find(sales_record_address_params[:item_id]).price,
+        card: sales_record_address_params[:token],
+        currency: 'jpy'
+      )
   end
 end
