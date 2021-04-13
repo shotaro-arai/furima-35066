952444acf455ea225aa692debc0cc3de07bef398
shotaro-arai <syoooth0720@yahoo.co.jp>
2021-04-13 16:09:16 +0900

    authenticate_user!メソッドのexceptにincremental_searchアクションを追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show, :search, :sort]
+  before_action :authenticate_user!, except: [:index, :show, :search, :incremental_search, :sort]
   before_action :set_item, only:[:show, :edit, :update, :destroy]
   before_action :move_action, only:[:edit, :update, :destroy]
 
